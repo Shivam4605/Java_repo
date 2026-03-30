b13cc81e946fa3bfe792533cfc42b2d6c46264cd
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-31 02:58:06 +0530

    commited on 31/3/2026

---
+++ basic_logic_array/Arrays/duplicate.java
@@ -0,0 +1,27 @@
+package basic_logic_array.Arrays;
+import java.util.*;
+
+
+// public class Duplicate {
+
+//     static int duplicate(int[] nums) {
+
+//         HashSet<Integer> set = new HashSet<>();
+//         Arrays.sort(nums);
+//         for (int i = 0; i < nums.length; i++) {
+//             if(!set.contains(nums[i])){
+//                 set.add(nums[i]);
+//             }
+//         }
+
+//         if(set.size() < 3){
+//             return set.iterator().next();
+//         }
+//         return -1; 
+//     }
+
+//     public static void main(String[] args) {
+//         duplicate(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 });
+//     }
+// }
+
